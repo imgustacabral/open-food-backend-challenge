69790039ca5bb3d6d11fafed5e6fc34b4dbe2278
imgustacabral <imgustacabral@gmail.com>
2023-07-15 14:40:50 -0300

    feat: add findAll method

--- src/application/repositories/products-repository.ts
+++ src/application/repositories/products-repository.ts
@@ -3,4 +3,5 @@ import { Product } from '@application/entities/product';
 export abstract class ProductsRepository {
   abstract findByCode(code: number): Promise<Product | null>;
   abstract save(product: Product): Promise<void>;
+  abstract findAll(pageNumber: number, pageSize: number): Promise<Product[]>;
 }
