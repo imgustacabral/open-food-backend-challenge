ccf475f483042a482784d8b5868d6a8b37b31a6b
imgustacabral <imgustacabral@gmail.com>
2023-07-16 14:26:54 -0300

    refactor: add null return on product entity

--- src/application/repositories/products-repository.ts
+++ src/application/repositories/products-repository.ts
@@ -1,7 +1,7 @@
 import { Product } from '@application/entities/product';
 
 export abstract class ProductsRepository {
-  abstract findByCode(code: number): Promise<Product | null>;
-  abstract save(product: Product): Promise<void>;
+  abstract findByCode(code: string): Promise<Product | null>;
+  abstract save(product: Product): Promise<Product>;
   abstract findAll(pageNumber: number, pageSize: number): Promise<Product[]>;
 }
