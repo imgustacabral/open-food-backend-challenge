ada11931327bdd926467d0cb06024f96f7ddf09e
imgustacabral <imgustacabral@gmail.com>
2023-07-17 07:55:05 -0300

    feat: add docker files

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:18.16-alpine
+
+WORKDIR /app
+
+RUN apk add --no-cache postgresql-client
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+CMD npx prisma migrate deploy && npx prisma generate && npm run build && npm run start
--- src/application/repositories/products-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from '@application/entities/product';
-
-export abstract class ProductsRepository {
-  abstract findByCode(code: string): Promise<Product | null>;
-  abstract save(product: Product): Promise<Product>;
-  abstract findAll(pageNumber: number, pageSize: number): Promise<Product[]>;
-}
