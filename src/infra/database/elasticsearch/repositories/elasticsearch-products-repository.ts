59ccdb3dbbd2f9deace08e46aae52788778c8f26
imgustacabral <imgustacabral@gmail.com>
2023-07-18 01:37:00 -0300

    feat: create index if not exists

--- src/infra/database/elasticsearch/repositories/elasticsearch-products-repository.ts
+++ src/infra/database/elasticsearch/repositories/elasticsearch-products-repository.ts
@@ -1,15 +1,24 @@
 import { Product } from '@application/product/entities/product';
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { ElasticsearchProductMapper } from '../mappers/elasticsearch-product-mapper';
 import { SearchProductsRepository } from '@application/product/repositories/search-repository';
 
 @Injectable()
 export class ElasticsearchProductsRepository
-  implements SearchProductsRepository
+  implements OnModuleInit, SearchProductsRepository
 {
   constructor(private readonly elasticsearchService: ElasticsearchService) {}
 
+  async onModuleInit() {
+    try {
+      if (
+        !(await this.elasticsearchService.indices.exists({ index: 'products' }))
+      ) {
+        await this.elasticsearchService.indices.create({ index: 'products' });
+      }
+    } catch {}
+  }
   async index(product: Product) {
     const productToIndex = ElasticsearchProductMapper.toPersistence(product);
     await this.elasticsearchService.index({
